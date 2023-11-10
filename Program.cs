b7a58a55b872ebcaa1e8cf1234db2e2545f031e7
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2023-11-10 11:40:51 -0300

    creating pessoa

--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("teste");
+﻿using studying_csharp.Models;
+
+Pessoa p = new Pessoa();

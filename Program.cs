9004ca91eb2aeb753e64a0827f0639f17e5dc129
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2023-11-10 22:39:06 -0300

    changes

--- Program.cs
+++ Program.cs
@@ -4,3 +4,11 @@ Pessoa p = new Pessoa();
 p.Nome = "Ronaldo";
 p.Idade = 200;
 Console.WriteLine(p.Nome);
+
+decimal valor = 1.900000m;
+
+double altura = 1.77;
+
+Console.WriteLine(valor);
+
+Console.WriteLine(altura.ToString("0.00000"));

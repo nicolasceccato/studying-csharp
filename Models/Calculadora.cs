85a3c34fc958fa5c769211afcb8c1237644607e9
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2023-11-14 11:32:00 -0300

    incrementando a calculadora

--- Models/Calculadora.cs
+++ Models/Calculadora.cs
@@ -23,4 +23,26 @@ public class Calculadora
         Console.WriteLine($"{x} ^ {y} = {Math.Pow(x, y)}");
     }
 
+    public void Seno(double angulo)
+    {
+        double radiano = angulo * Math.PI / 180;
+        Console.WriteLine($"Seno de {angulo} = {Math.Sin(radiano)}");
+    }
+    public void Coseno(double angulo)
+    {
+        double radiano = angulo * Math.PI / 180;
+        Console.WriteLine($"Coseno de {angulo} = {Math.Cos(radiano)}");
+    }
+    public void Tangente(double angulo)
+    {
+        double radiano = angulo * Math.PI / 180;
+        Console.WriteLine($"Tangente de {angulo} = {Math.Tan(radiano)}");
+    }
+
+    public void RaizQuadrada(double valor)
+    {
+        double raiz = Math.Sqrt(valor);
+        Console.WriteLine($"Raiz de {valor} = {Math.Round(raiz, 4)}");
+    }
+
 }

d480df564ad0de3962e51455a5c3fb4946d9ebf8
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2023-12-07 17:52:56 -0300

    adicionando classe conta

---
+++ Models/Conta.cs
@@ -0,0 +1,13 @@
+﻿namespace studying_csharp.Models;
+
+public abstract class Conta
+{
+    protected decimal Saldo;
+
+    public void ExibirSaldo()
+    {
+        Console.WriteLine($"O seu saldo é igual a {Saldo}");
+    }
+
+    public abstract void Creditar(decimal valor);
+}
